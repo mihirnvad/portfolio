e3b7c469f1690cc25bfa6625463550e1084def82
mihirnvad <vadmihir09@gmail.com>
2026-04-17 17:45:40 -0700

    Update global.js

--- global.js
+++ global.js
@@ -3,3 +3,11 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+
+navLinks.forEach(link => {
+  if (link.href === window.location.href) {
+    link.classList.add("current");
+  }
+});

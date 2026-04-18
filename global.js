3e381b33ea25250031a602ef4dc47d39cae82613
mihirnvad <vadmihir09@gmail.com>
2026-04-17 18:09:51 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,8 +4,30 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+];
 
-let currentLink = navLinks.find((a) => a.host === location.host && a.pathname === location.pathname);
+const BASE_PATH =
+  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/website/";
 
-currentLink?.classList.add("current");
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let isCurrent =
+    location.pathname === BASE_PATH + p.url;
+
+  nav.insertAdjacentHTML(
+    'beforeend',
+    `<a href="${url}" class="${isCurrent ? 'current' : ''}">${p.title}</a>`
+  );
+}

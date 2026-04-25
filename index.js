bd5271c690343599fdb187e41323cccec8e42633
mihirnvad <vadmihir09@gmail.com>
2026-04-24 22:59:47 -0700

    bunch of updates

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
-
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('mihirnvad');
+
+const profileStats = document.querySelector('#profile-stats');
+
+console.log(githubData);
+console.log(profileStats);

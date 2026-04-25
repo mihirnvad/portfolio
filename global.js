b350ae69277967bda0bbcbfa4fbf5f89614419d5
mihirnvad <vadmihir09@gmail.com>
2026-04-24 22:35:50 -0700

    projects json

--- global.js
+++ global.js
@@ -74,3 +74,20 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
